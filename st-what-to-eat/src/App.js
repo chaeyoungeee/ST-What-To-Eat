8e3339cf875d059e48666f538015820ac3cc4687
Limchaeyoung <102286483+chaeyoungeee@users.noreply.github.com>
2023-10-29 08:43:38 +0900

    메인페이지 수정

--- st-what-to-eat/src/App.js
+++ st-what-to-eat/src/App.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { memo, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect } from "react";
-
+import memoji from './imgs/memoji.gif';
 import MainNav from "./components/MainNav";
 
 // react-bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 import "./App.scss";
 
 function App() {
+  let [up0, setUp0] = useState('')
+  let [up1, setUp1] = useState('')
+  let [up2, setUp2] = useState('')
+  let [fade, setFade] = useState('')
+
+  useEffect(()=>{
+    setTimeout(()=>{ setUp0('up')}, 100)
+    setTimeout(()=>{ setUp1('up')}, 500)
+    setTimeout(()=>{ setUp2('up')}, 1000)
+    setTimeout(()=>{ setFade('end')}, 1500)
+
+    return ()=>{
+      setUp0('')
+      setUp1('')
+      setUp2('')
+      setFade('')
+    }
+  }, [])
   return (
     <div className="App">
-      <MainNav />
+      <div className={"start "+ fade}><MainNav /></div>
+      <div className="main">
+          <div className="title">
+            <h1 className={"bottom "+ up1}>과기대에서</h1>
+            <h1 className={"bottom "+ up2}>오늘</h1>
+            <h1 className={"bottom "+ up2}>뭐 먹지?</h1>
+          </div>
+           <img className={"bottom "+ up0} src={memoji}></img>
+      </div>
     </div>
   );
 }
