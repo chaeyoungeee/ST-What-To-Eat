2d05efaeaf0acbf970fe4d943c63f89ccdf196b6
Limchaeyoung <102286483+chaeyoungeee@users.noreply.github.com>
2023-10-29 09:17:46 +0900

    메인페이지 배치 수정

--- st-what-to-eat/src/App.js
+++ st-what-to-eat/src/App.js
@@ -35,11 +35,12 @@ function App() {
       <div className={"start "+ fade}><MainNav /></div>
       <div className="main">
           <div className="title">
-            <h1 className={"bottom "+ up1}>과기대에서</h1>
-            <h1 className={"bottom "+ up2}>오늘</h1>
-            <h1 className={"bottom "+ up2}>뭐 먹지?</h1>
+            <div className={"bottom "+ up1}>과기대에서</div>
+            <div className={"bottom "+ up2}>오늘</div>
+            <div className={"bottom "+ up2}>뭐 먹지?</div>
+            <img className={"bottom " + up0} src={memoji}></img>
           </div>
-           <img className={"bottom "+ up0} src={memoji}></img>
+          
       </div>
     </div>
   );
